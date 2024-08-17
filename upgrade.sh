43c69e6727ff06ca89113f40fcb943ac596c3556
Till Tomczak <96786986+tilltmk@users.noreply.github.com>
2024-08-18 00:41:12 +0200

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 
-# Sicherstellen, dass das System vollständig aktualisiert ist
+# System auf den neuesten Stand bringen
 sudo zypper refresh
 sudo zypper update
 
-# Wechseln zu einem virtuellen Terminal (außerhalb von X-Window, um Abstürze zu vermeiden)
-echo "Bitte wechseln Sie zu einem virtuellen Terminal (z.B. mit Ctrl+Alt+F3) und führen Sie das Skript erneut aus."
-read -p "Drücken Sie Enter, um fortzufahren..."
+# Überprüfen, ob die Repositories bereits auf $releasever eingestellt sind
+sudo sed -i 's/15.4/${releasever}/g' /etc/zypp/repos.d/*.repo
 
-# Überprüfen und Aktivieren der notwendigen Repositories
-sudo zypper repos -u
+# Repositories auf die neue Version aktualisieren
+sudo zypper --releasever=15.5 refresh
 
-# Repositories für Leap 15.5 hinzufügen und aktivieren
-sudo zypper modifyrepo --all --disable
-sudo zypper addrepo --check --refresh http://download.opensuse.org/distribution/leap/15.5/repo/oss/ repo-oss
-sudo zypper addrepo --check --refresh http://download.opensuse.org/distribution/leap/15.5/repo/non-oss/ repo-non-oss
-sudo zypper addrepo --check --refresh http://download.opensuse.org/update/leap/15.5/oss/ repo-update
-
-# Repositories aktualisieren
-sudo zypper refresh
-
-# System-Upgrade durchführen
+# Upgrade der Distribution durchführen
 sudo zypper --releasever=15.5 dup --download-in-advance
 
-# Nach dem Upgrade neustarten
-echo "Upgrade abgeschlossen. Das System wird jetzt neu gestartet."
-sudo reboot
+# Nach dem Upgrade Neustart durchführen
+echo "Upgrade abgeschlossen. System wird neu gestartet."
+# sudo reboot
